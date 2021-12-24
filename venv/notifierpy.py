a4b279baf809719578b70c531a583b47af107630
rono516 <ronocollins2000@gmail.com>
2021-12-24 23:24:32 +0300

    window

--- venv/notifierpy.py
+++ venv/notifierpy.py
@@ -4,9 +4,9 @@ from tkinter import messagebox
 from PIL import Image, ImageTk
 import time
 
-t = Tk()
-t.title('Notifier')
-t.geometry("500x300")
+twindow = Tk()
+twindow.title('Notifier')
+twindow.geometry("500x300")
 img = Image.open("notify-label.png")
 tkimage = ImageTk.PhotoImage(img)
 
@@ -23,7 +23,7 @@ def get_details():
         int_time = int(float(get_time))
         min_to_sec = int_time * 60
         messagebox.showinfo("notifier set", "set notification ?")
-        t.destroy()
+        twindow.destroy()
         time.sleep(min_to_sec)
 
         notification.notify(title=get_title,
@@ -33,42 +33,42 @@ def get_details():
                             toast=True,
                             timeout=10)
 
-img_label = Label(t, image=tkimage).grid()
+img_label = Label(twindow, image=tkimage).grid()
 
 # Label - Title
-t_label = Label(t, text="Title to Notify",font=("poppins", 10))
+t_label = Label(twindow, text="Title to Notify",font=("poppins", 10))
 t_label.place(x=12, y=70)
 
 # ENTRY - Title
-title = Entry(t, width="25",font=("poppins", 13))
+title = Entry(twindow, width="25",font=("poppins", 13))
 title.place(x=123, y=70)
 
 # Label - Message
-m_label = Label(t, text="Display Message", font=("poppins", 10))
+m_label = Label(twindow, text="Display Message", font=("poppins", 10))
 m_label.place(x=12, y=120)
 
 # ENTRY - Message
-msg = Entry(t, width="40", font=("poppins", 13))
+msg = Entry(twindow, width="40", font=("poppins", 13))
 msg.place(x=123,height=30, y=120)
 
 # Label - Time
-time_label = Label(t, text="Set Time", font=("poppins", 10))
+time_label = Label(twindow, text="Set Time", font=("poppins", 10))
 time_label.place(x=12, y=175)
 
 # ENTRY - Time
-time1 = Entry(t, width="5", font=("poppins", 13))
+time1 = Entry(twindow, width="5", font=("poppins", 13))
 time1.place(x=123, y=175)
 
 # Label - min
-time_min_label = Label(t, text="min", font=("poppins", 10))
+time_min_label = Label(twindow, text="min", font=("poppins", 10))
 time_min_label.place(x=175, y=180)
 
 # Button
-but = Button(t, text="SET NOTIFICATION", font=("poppins", 10, "bold"), fg="#ffffff", bg="#528DFF", width=20,
+but = Button(twindow, text="SET NOTIFICATION", font=("poppins", 10, "bold"), fg="#ffffff", bg="#528DFF", width=20,
              relief="raised",
              command=get_details)
 but.place(x=170, y=230)
 
-t.resizable(0,0)
-t.mainloop()
+twindow.resizable(0,0)
+twindow.mainloop()
 
